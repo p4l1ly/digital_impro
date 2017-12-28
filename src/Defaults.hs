a7bd8ddea73205d16b9459d1e7a84714090a6cc7
Juraj Čarnogurský <durisvk2@gmail.com>
2017-12-28 23:17:42 +0100

    connecting

--- src/Defaults.hs
+++ src/Defaults.hs
@@ -1,7 +1,7 @@
 module Defaults where
 
-minLength = 300 :: Int
-maxLength = 1050 :: Int
+minLength = 10 :: Int
+maxLength = 30 :: Int
 initVelocity = 3e-5 :: Double
 accelerationFormula = "6e-5 + 3e-5*(a + b)"
 angularAccelerationFormula = "4e-1 + 2e-2*(a - b)"
--- web/src/core/driver.js
+++ web/src/core/driver.js
@@ -1,5 +1,5 @@
 const { spawn } = require('child_process');
-
+const { EOL } = require('os');
 const config = require('../config');
 
 const { join } = require('path');
@@ -8,13 +8,34 @@ const binary = join(__dirname, '..', config.binary);
 
 
 module.exports = () => {
-    const bin = spawn(binary);
+    const bin = spawn(binary, [], {
 
+    });
+    bin.stdin.setEncoding('utf-8');
+    bin.stdout.setEncoding('utf-8');
 
     bin.stdin.write(JSON.stringify({ event: 'generate' }) + '\n');
 
+    let response = '';
+
+    let charsc = 0;
+
+
     bin.stdout.on('data', (chunk) => {
-        console.log(JSON.parse(chunk.toString()));
+        //response += chunk.toString();
+        
+        //console.log(chunk.toString().length);
+        console.log(chunk);
+        //charsc += chunk.toString().length;
+        //console.log(charsc);
+        //return true;
+        /*if(chunk.toString().endsWith('\n')) {
+            console.log('endswith');
+            console.log(response);
+        }*/
     });
 
+    bin.stderr.on('data', (chunk) => {
+        console.log('err', chunk);
+    })
 }
